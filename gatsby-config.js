f68db652744279b4e17df8d612e78aee72761636
Giorgio <giorgio.valbonesi@gmail.com>
2021-03-03 12:41:33 +0100

    Add google analytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,18 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [`gatsby-plugin-postcss`, `gatsby-plugin-fontawesome-css`, `gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-postcss`, 
+    `gatsby-plugin-fontawesome-css`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-XQ1CFXHJLF", // Google Analytics / GA
+        ],
+      },
+    },
+  ],
 }
