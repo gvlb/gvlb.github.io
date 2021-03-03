966d59860439a95a3509a631a83352e936988a1d
Giorgio <giorgio.valbonesi@gmail.com>
2021-03-03 11:00:48 +0100

    Added react plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,5 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [`gatsby-plugin-postcss`, `gatsby-plugin-fontawesome-css`],
+  plugins: [`gatsby-plugin-postcss`, `gatsby-plugin-fontawesome-css`, `gatsby-plugin-react-helmet`],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Project from "./Project"
+import Project from "../components/Project"
 
 import dezoneicon from '../images/dezone.svg'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
