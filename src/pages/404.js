6c85079ac05393c48eb2fd9ebd365d00721821a8
Giorgio <giorgio.valbonesi@gmail.com>
2021-09-11 17:54:09 +0200

    Fix an issue where wouldn't show styles in production

---
+++ src/pages/404.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Page404() {
+    // create a form connected to some FaaS or even like Zapier or something
+    // https://medium.com/datafire-io/simple-backends-four-ways-to-implement-a-contact-us-form-on-a-static-website-10fc430984a4
+    return <h1>404 - Not found</h1>
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: 'false',
+  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: 'media', // or 'media' or 'class'
   theme: {
     fontFamily: {
